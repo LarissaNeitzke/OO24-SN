e76a7ffa2025e38ac9c1c47abd28503e68207505
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2025-03-07 19:42:17 -0300

    Update organizador.ts

--- Aula 05 - jogosVorazes/organizador.ts
+++ Aula 05 - jogosVorazes/organizador.ts
@@ -25,16 +25,24 @@ public getDistritos(): Distrito[] {
     return this.distritos;
 }
 
+public getDistritosNomes(): string {
+    let distritosNomes = [];
+    for (let i=0; i < this.distritos.length; i++) {
+        distritosNomes.push(this.distritos[i].getNome())
+    }
+    return distritosNomes.join(", ")
+}
+
 public liberarAnimais(): void {
     const distritoAleatorio = Math.floor(Math.random() * this.distritos.length);
-    console.log(`O Organizador ${this.nome} soltou animais no Distrito ${this.distritos[distritoAleatorio]}! Corra!`)
+    console.log(`O Organizador ${this.nome} soltou animais no Distrito ${this.distritos[distritoAleatorio].getNome()}! Corra!`)
 }
 
 public criarDesastre(): void {
     let listaDesastre = ['Chuva Ácida', 'Terremoto', 'Fumaça Tóxica', 'Lanças de Fogo']
     const desastreAleatorio = Math.floor(Math.random() * listaDesastre.length);
     const distritoAleatorio = Math.floor(Math.random() * this.distritos.length);
-    console.log(`O Organizador ${this.nome} liberou o Desastre ${listaDesastre[desastreAleatorio]} no Distrito ${this.distritos[distritoAleatorio]}!`)
+    console.log(`O Organizador ${this.nome} liberou o Desastre ${listaDesastre[desastreAleatorio]} no Distrito ${this.distritos[distritoAleatorio].getNome()}!`)
 }
 
 }
