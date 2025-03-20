abacd5b5c8d65209958af8f11b9d2ee8e079e505
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2025-03-20 15:12:30 -0300

    Update Animal.ts

--- Aula 10 - 18-03-2025/Animal.ts
+++ Aula 10 - 18-03-2025/Animal.ts
@@ -1,7 +1,6 @@
-import { Recurso } from "./Recursos";
+import { Recurso } from "./Recurso";
 
-
-export class Animal {
+export abstract class Animal {  // Abstract!!!
     protected nome:string;
     protected especie:string;
     protected fome:boolean;
@@ -71,58 +70,55 @@ export class Animal {
     }
 
     // Métodos diversos
-    public buscarRecurso(listaRecursos): boolean {
+    public buscarRecurso(): void {
         console.log(`> ${this.nome} (${this.especie}) está à ${this.habito.toLowerCase()}...`);
-        return this.verficarLoot(listaRecursos);
-    }
 
-    private verficarLoot(listaRecursos): boolean {
-        const achouLoot = Math.floor(Math.random() * 2);
-        if (achouLoot == 1) {
+        const achouLoot = Math.floor(Math.random() * 2);                            // Aleatoriza de 0 a 1 para definir se o animal achou um item. 
 
-            const indexAleatorio = Math.floor(Math.random() * listaRecursos.length);
-            const lootObtido = listaRecursos[indexAleatorio];
-            Animal.inventario.push(lootObtido);
+        if (achouLoot == 1) {                                                       // Caso '1', o seguinte acontece:
+            const recursos = Recurso.getRecursos();                                 // Pega a LISTA dos possíveis recursos.
+            const indexAleatorio = Math.floor(Math.random() * recursos.length);     // Roleta um número de 0 até a QUANTIDADE-1 de possíveis recursos.
+            const lootObtido = recursos[indexAleatorio];                            // Gera um aleatório recurso.
+            Animal.inventario.push(lootObtido);                                     // Armazena o aleatório recurso gerado no atributo static 'inventario'.
 
-            console.log(`[✔] ${this.nome} encontrou ${lootObtido}!`, '\n');
-            return true;
-            
-        } else {
-            
+            console.log(`[✔] ${this.nome} encontrou ${lootObtido.getNome()}!`, '\n');
+
+        } else {                                                                    // Caso '0', somente uma mensagem aparece.
             console.log(`[✗] ${this.nome} não encontrou nada.`, '\n');
-            return false;
         }
     }
 
-    public usarRecurso(recursoRecebido) {
-        const consultarInventario = Animal.consultaInventario(recursoRecebido)
-        console.log(`${this.nome} ${consultarInventario}`)
-    }
+    public usarRecurso(recursoRecebido:Recurso):void {
+        console.log(`> ${this.nome} deseja ${recursoRecebido.getNome()}...`)
 
-    public static consultaInventario(recursoRecebido):string {
-        if (this.inventario.includes(recursoRecebido)) {
-            return `está à ${recursoRecebido.funcao} ${recursoRecebido.quantidade} ${recursoRecebido.nome}`
-        } else {
-            return `não possui o recurso ${recursoRecebido.nome}`
+        if (Animal.inventario.includes(recursoRecebido)) {                          //  Se, no inventário compartilhado, há o Recurso recebido:
+
+            if (recursoRecebido.getFuncao() == "comer") {                           //      Se o recurso for comestível, o animal perde a fome. 
+                this.fome = false;                                                              
+            } else if (recursoRecebido.getFuncao() == "beber") {                    //      Se o recurso for bebível, o animal perde a sede.
+                this.sede = false;
+            }
+            console.log(`[✔] Obteve a partir do inventário e está à ${recursoRecebido.getFuncao()} ${recursoRecebido.getQuantidade()} ${recursoRecebido.getNome()}!`, '\n');
+
+        } else {                                                                    //  Caso contrário, somente uma mensagem aparece.
+            console.log(`[✗] Porém não foi encontrado no inventário!`, '\n');
         }
     }
 
-    protected emitirSom(): string {
-        return `> ${this.nome} (${this.especie}) fez barulho!`
-    }
+
+    // Abstract: É bem fácil! Simplesmente OBRIGA subclasses á terem seus próprios métodos 'emitirSom()'. Só isso.
+    // Caso a subclasse não venha a implementar seu próprio, vai mostrar um sublinhado vermelho, como se tivesse um erro.
+    // (Assim, também, a gente consegue reutilizar o mesmo nome de método. Nao precisando criar um 'emitirSomMamifero')
+    public abstract emitirSom():void;
 
     public mover(): void {
-        if (this.sede) {
-            console.log(`> ${this.nome} (${this.especie}) tentou se mover, porém está com sede!`, '\n');
-        } else {
-            console.log(`> ${this.nome} (${this.especie}) se moveu!`, '\n');
-            this.sede = true;
-        }
-    }
+        console.log(`> ${this.nome} (${this.especie}) tenta correr...`)
 
-    public descansar():void {
-        console.log(`> ${this.nome} (${this.especie}) acaba de descansar. Sua fome e sede foram revigoradas!`, '\n');
-        this.sede = false;
-        this.fome = false;
+        if (this.sede) {                                                            // Se 'sede' for 'true', somente uma mensagem aparece.
+            console.log(`[✗] Porém, por estar sedento, não foi capaz!`, '\n');
+        } else {                                                                    // Caso contrário, 'sede' recebe 'true'.
+            this.sede = true;                                                       
+            console.log(`[✔] A hidratação está em dia! ${this.nome} começou a correr!`, '\n');
+        }
     }
 }
