0f253140c4e5961cf1a5a93c4130e39a617855d6
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2025-05-08 21:57:08 -0300

    Add files via upload

---
+++ Atividade Marvel (Aula 22)/FilaTemporal.ts
@@ -0,0 +1,31 @@
+import { PersonagemTemporal } from "./PersonagemTemporal";
+
+class FilaTemporal {
+    private personagens:PersonagemTemporal[] = []
+
+    enfileirar(personagem: PersonagemTemporal):void{
+        this.personagens.push(personagem);
+    }
+    desenfileirar(): PersonagemTemporal | undefined {
+        if (this.estaVazia()){
+            console.log("A fila está vazia.");
+            return undefined;
+        } return this.personagens.shift();
+    }
+    primeiroDaFila(): PersonagemTemporal | undefined {
+        if (this.estaVazia()){
+            console.log("A fila está vazia.");
+            return undefined;
+        }
+        return this.personagens[0];
+    }
+    estaVazia(): boolean {
+        return this.personagens.length === 0;
+    }
+    tamanho(): number{
+        return this.personagens.length;
+    }
+    exibirFila():void {
+        console.log(this.personagens)
+    }
+}
---
+++ Atividade Marvel (Aula 22)/PersonagemTemporal.ts
@@ -0,0 +1,20 @@
+export class PersonagemTemporal {
+    private nome:string
+    private universo:string
+    assinaturaTemporal:number
+
+
+    constructor(nome:string, universo:string, assinaturaTemporal:number){
+        this.nome = nome
+        this.universo = universo
+        this.assinaturaTemporal = assinaturaTemporal
+    }
+
+    public getAssinaturaTemporal():number{
+        return this.assinaturaTemporal
+    }
+
+    public exibirInfo(){
+        console.table(this)
+    }
+}
